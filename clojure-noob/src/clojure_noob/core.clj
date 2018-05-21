2a8ef06a75db71ecf80b0cf2f907074c567fe839
Luiz Felipe (lflimeira) <lflimeira02@gmail.com>
2018-05-21 14:52:28 -0300

    hello world iterator

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,7 +1,15 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defn hello-world 
+  [n]
+  (loop [iteration 1]
+    (println "Hello World.")
+    (if (= iteration n)
+      (println "Goodbye!")
+      (recur (inc iteration)))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "I'm a little teapot!"))
+  []
+  (hello-world 5))
+
